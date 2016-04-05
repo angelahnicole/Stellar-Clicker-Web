dbfc51bf3091dc9e2e330040018dfcedcad63a35
angelahnicole <angelangross@gmail.com>
2016-04-04 19:23:46 -0600

    Adding flarum to the project

---
+++ flarum/config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'sc_web',
+    'username' => 'sc',
+    'password' => '68$NXdqkVvC634@m%Im#',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'flarum_',
+    'strict' => false,
+  ),
+  'url' => 'http://forum.stellar.polymorphixgaming.com',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
