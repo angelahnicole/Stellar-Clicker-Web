174fdd06334562f00d1fddcc3a60070208d2776b
angelahnicole <angelangross@gmail.com>
2016-04-10 01:28:33 -0600

    Fixing notification

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -3,67 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Routing\Controller as LaravelBaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 // ===============================================================================================================
-// Home Controller
+// 
 // ---------------------------------------------------------------------------------------------------------------
 // Stellar Clicker
 // https://github.com/angelahnicole/Stellar-Clicker-Web
 // Angela Gross
 // ---------------------------------------------------------------------------------------------------------------
-// Controller for all of the different homepages for the website. (Except blog, as that has its own controllers)
+// 
 // ===============================================================================================================
-
-class HomeController extends BaseController
+class BaseController extends LaravelBaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    
-    // ------------------------------------------------------------------------------------------------------------------------------
-    // ATTRIBUTES
-    // ------------------------------------------------------------------------------------------------------------------------------
-	
-    protected $data = array();
-	
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public static function processNotifications()
+    {
+        $successNotes = session('successNotes');
+        $errorNotes = session('errorNotes');
         
-    // ------------------------------------------------------------------------------------------------------------------------------
-    // VIEW CREATORS 
-    // ------------------------------------------------------------------------------------------------------------------------------
-	
-    /**
-     * Creates home page view for end-user.
-     *
-     * @return View (stellar home page)
-     */
-    public function stellarHome()
-    {
-        // Get basic information for home page
-        $this->data['title'] = "Stellar Clicker: The Game";
-        $this->data['showDefaultNotifcations'] = true;
-        $this->data['showNav'] = false;
-
-        return view('home.home', $this->data);
-    }		
-    
-    /**
-     * Creates home page view for end-user.
-     *
-     * @return View (wiki home page)
-     */
-    public function wikiHome()
-    {
-        // Get basic information for home page
-        $this->data['title'] = "Stellar Clicker: The Wiki";
-        $this->data['showDefaultNotifcations'] = true;
-        $this->data['showNav'] = true;
-
-        return view('wiki.home', $this->data);
+        if($successNotes)
+        {
+            foreach($successNotes as $notification)
+            {
+                \Notification::successInstant($notification);
+            }
+        }
+        
+        if($errorNotes)
+        {
+            foreach($errorNotes as $notification)
+            {
+                \Notification::errorInstant($notification);
+            }
+        }
+        
+        session()->forget('successNotes');
+        session()->forget('errorNotes');
     }
     
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public static function addNotification($notification, $type)
+    {
+        switch($type)
+        {
+            case 'success': session()->push('successNotes', $notification); break;
+            case 'error': session()->push('errorNotes', $notification); break;
+        }
+    }
 }
--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title><?php echo (isset($title) ? $title :'Stellar Clicker: The Game'); ?> - Polymorphix Gaming</title>
+    <title><?php echo (isset($title) ? $title :'Stellar Clicker: The Game'); ?> - Stellar Clicker - Polymorphix Gaming</title>
         
     <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('imx/favicon.ico')}}" />
+    
 
     @include('master._partials.assets')
     @yield('assets')
@@ -21,12 +23,11 @@
     <!-- /NAVIGATION BAR -->
     
     
-    @if(isset($showDefaultNotifcations) && $showDefaultNotifcations)
+    @if(isset($showNotifications) && $showNotifications)
         <div class="container-fluid">   
             <div class="row">
                 <div class="col-md-12">
-                    {{-- NOTIFICATIONS --}}
-                     @include('master._partials.notifications')               
+                    @notification()              
                 </div>
             </div>
         </div>
